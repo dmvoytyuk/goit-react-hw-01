de6e87877c27831a94f46efa636a6752567a52d5
dmvoytyuk <dmvoytyuk@gmail.com>
2024-03-09 19:41:54 +0100

    all tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
+import Profile from '../components/Profile/Profile';
+import FriendList from '../components/FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import userData from '../data/userData.json';
+import friends from '../data/friends.json';
 
+import transactions from '../data/transactions.json'
 function App() {
 
   return (
     <>
-     <h1>Hello World!</h1>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
-  )
+  );
 }
 
 export default App
